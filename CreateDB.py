0d79770c7d9ca89315607b599c203c2183d26b28
JRArocha <99799580+JRArocha@users.noreply.github.com>
2023-07-12 15:50:56 +0800

    Add files via upload

---
+++ CreateDB.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE isecure")
+mycursor.execute("SHOW DATABASES")
+
+mydb1 = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  database="isecure"
+)
+
+mycursor1 = mydb1.cursor()
+mycursor1.execute("CREATE TABLE logindb1 (ID INT(11), Username TEXT, Password TEXT, API TEXT, Directory TEXT, Camera TEXT, RTSP TEXT)")
+mycursor1.execute("SHOW TABLES")
