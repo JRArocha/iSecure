03896ffe7942b63338af5da98f7fc7b5b2dcdea1
JRArocha <99799580+JRArocha@users.noreply.github.com>
2023-07-12 17:46:46 +0800

    Create Database

--- CreateDB.py
+++ CreateDB.py
@@ -19,3 +19,15 @@ mydb1 = mysql.connector.connect(
 mycursor1 = mydb1.cursor()
 mycursor1.execute("CREATE TABLE logindb (ID INT(11), Username TEXT, Password TEXT, API TEXT, Directory TEXT, Camera TEXT, RTSP TEXT)")
 mycursor1.execute("SHOW TABLES")
+
+mydb2 = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  database="isecure",
+  table="logindb"
+)
+
+mycursor2 = mydb2.cursor()
+sql = "INSERT INTO logindb (Username, Password, Camera) VALUES (%s, %s, %s)"
+val = ("ADMIN", "ADMIN", 0)
+mycursor2.execute(sql, val)
